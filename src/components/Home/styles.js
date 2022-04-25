38056c49b3145daeac334cc27d0d6ee2d1aff16c
OgakiP <pauloogaki.principal@gmail.com>
2022-04-25 20:44:23 -0300

    feat: feat remove default files and create home directories and files

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div`
- background-color: ${props => props.theme.colors.background};
+    background-color: ${props => props.theme.colors.background};
 `
