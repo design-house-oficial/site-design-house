b892a6e128f3202035f5b314be44ec6e66c25de4
ogaki <pauloogaki.principal@gmail.com>
2022-04-28 18:23:37 -0300

    feature: create navbar component and create logo letters files

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,10 @@
+import * as S from "./styles"
+
+const NavBar = () => {
+    return (
+        <S.Wrapper>
+
+        </S.Wrapper>
+    )
+}
+export default NavBar
---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+    height: 94px;
+    background-color: ${props => props.theme.colors.backgrround};
+`
