8fadc322a1a6270c13fe63706a35d68202aa55af
ogaki <pauloogaki.principal@gmail.com>
2022-04-28 19:34:25 -0300

    feat: create stylized navbar on mobile

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 export const Wrapper = styled.header`
   height: 94px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 15px 0px 15px;
   background-color: ${(props) => props.theme.colors.backgrround};
 `;
 
