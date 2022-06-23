5c09b1d36ea47001ad3fa340c6531afb9f495f78
ogaki <pauloogaki.principal@gmail.com>
2022-06-23 14:22:15 -0300

    feature/change the header logo import to public folder

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,13 +1,12 @@
 import * as S from "./styles"
 import Image from "next/image"
 import menuHamb from "../../assets/svg/menu-icon.svg"
-import logo from "../../assets/svg/logo.png"
 
 const NavBar = () => {
     return (
         <S.Wrapper>
             <S.LogoContainer>
-                <Image src={logo.src} width="120px" height="45px"/>
+                <Image src="/logo.png" width="120px" height="45px"/>
             </S.LogoContainer>
             <S.HambContainer>
                 <Image src={menuHamb} alt="menuHamb"/>
