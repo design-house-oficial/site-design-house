d463ec0dcb94d10d8ab116c74cfa0f9a5ceab3b7
ogaki <pauloogaki.principal@gmail.com>
2022-06-27 20:02:16 -0300

    feature/add rd station

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='pt-br'>
+      <Head>
+        <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/87091806-26c1-4876-a64b-6be169691ed1-loader.js"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
